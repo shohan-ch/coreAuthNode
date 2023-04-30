ca52fc1699324b4b0e8019696739a0101cbf0129
shohan-ch <shohan889@yahoo.com>
2023-05-01 02:24:01 +0600

    Validation done

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,4 +1,5 @@
 const { formData } = require("../lib/formData");
+const { validate } = require("../lib/validate");
 exports.login = (res) => {
   res.end("Login page");
 };
@@ -7,12 +8,13 @@ exports.register = async (req, res) => {
   try {
     let data = await formData(req);
     const { name, email } = data;
-    if (name == "") {
-      res.end(
-        JSON.stringify({ message: "Name should not be empty!", status: false })
-      );
-    }
-    console.log(data.name);
+    res.end(validate({ name: "required" }));
+    // if (name == "") {
+    //   res.end(
+    //     JSON.stringify({ message: "Name should not be empty!", status: false })
+    //   );
+    // }
+    console.log(name);
     res.end("Register page");
   } catch (error) {
     console.error(error);
