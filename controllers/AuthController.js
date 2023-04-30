c2db9aa3329c9ffb07b7a4044288942b84c3076c
shohan-ch <shohan889@yahoo.com>
2023-05-01 01:32:00 +0600

    Validation added

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,11 +1,20 @@
-const { parse } = require("querystring");
 const { formData } = require("../lib/formData");
 exports.login = (res) => {
   res.end("Login page");
 };
 
 exports.register = async (req, res) => {
-  let data = await formData(req);
-  console.log(data.name);
-  res.end("Register page");
+  try {
+    let data = await formData(req);
+    const { name, email } = data;
+    if (name == "") {
+      res.end(
+        JSON.stringify({ message: "Name should not be empty!", status: false })
+      );
+    }
+    console.log(data.name);
+    res.end("Register page");
+  } catch (error) {
+    console.error(error);
+  }
 };
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const http =  require('http');
-const { route } = require('./routes/route');
-const server = http.createServer(route)
+const http = require("http");
+const { route } = require("./routes/route");
+const server = http.createServer(route);
 
-server.listen('3000',()=>{
-    console.log('Server listen at 8000')
-})
+server.listen("3000", () => {
+  console.log("Server listen at 8000");
+});
