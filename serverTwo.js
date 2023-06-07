36c4918b1997f6bc5fbb893a8c2dbf513aa49175
shohan-ch <shohan889@yahoo.com>
2023-06-07 23:10:14 +0600

    Https declare

--- serverTwo.js
+++ serverTwo.js
@@ -1,1 +1,12 @@
 const https = require("https");
+
+const server = https.createServer((req, res) => {
+  console.log(req);
+  if (req.url == "/") {
+    res.end("Https Server created");
+  }
+});
+
+server.listen(3000, () => {
+  console.log(`Server listen at port:3000`);
+});
