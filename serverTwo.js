7b12e6a752160cfce76a80dffed2dc65c8e1d6d3
shohan-ch <shohan889@yahoo.com>
2023-06-08 12:38:02 +0600

    some code changes

--- serverTwo.js
+++ serverTwo.js
@@ -1,12 +1,18 @@
-const https = require("https");
+const {createServer} = require("https");
+const {readFileSync} =  require('fs') 
 
-const server = https.createServer((req, res) => {
-  console.log(req);
-  if (req.url == "/") {
+
+const option =  {
+      key: readFileSync("./key.pem", 'utf8'),
+      cert: readFileSync("./cert.pem", 'utf8'),
+    };
+
+const server = createServer( option, (req, res) => {
     res.end("Https Server created");
-  }
+  
 });
 
-server.listen(3000, () => {
+server.listen(3001, () => {
   console.log(`Server listen at port:3000`);
 });
+
