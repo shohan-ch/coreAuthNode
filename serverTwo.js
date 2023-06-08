c1c34069730095d1860a0253aa859e0cae4e3e7c
shohan-ch <shohan889@yahoo.com>
2023-06-09 01:02:53 +0600

    External api call done by axios

--- serverTwo.js
+++ serverTwo.js
@@ -1,15 +1,25 @@
 const { createServer } = require("https");
 const { readFileSync } = require("fs");
+const { default: axios } = require("axios");
 
+// SSL certificate create for localhost
 const option = {
   key: readFileSync("./sslKey/localhost.key"),
   cert: readFileSync("./sslKey/localhost.crt"),
 };
 
-// console.log(option);
-const server = createServer(option, (req, res) => {
-  res.writeHead(200);
-  res.end("Hello from Node!\n");
+// https module server
+const server = createServer(option, async (req, res) => {
+  try {
+    let response = await axios.get(
+      "https://jsonplaceholder.typicode.comss/posts/1"
+    );
+    res.writeHead(200);
+    res.end(JSON.stringify(response.data));
+  } catch (error) {
+    console.log(error);
+    console.log("error is:", error.message);
+  }
 });
 
 server.listen(3000, () => {
