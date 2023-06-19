a043e94f4d9a90953bae831dfe2b83e72f6bd620
shohan-ch <shohan889@yahoo.com>
2023-06-19 23:25:14 +0600

    Directory created in two ways by fs module

--- controllers/FIleController.js
+++ controllers/FIleController.js
@@ -1,5 +1,23 @@
+const { mkdir } = require("fs/promises");
 const fs = require("fs");
 
-exports.creteFolder = (res) => {
-  res.end("File controller");
+exports.creteFolder = async (res) => {
+  // Directory Created in syncronus callback way
+  fs.mkdir(__dirname + "/old", { recursive: true }, (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Created Folder");
+    }
+  });
+
+  // Directory Created in asyncronous promise way
+  try {
+    let currentDirectory = __dirname;
+    let folder = mkdir(`${currentDirectory}/new`, { recursive: true });
+    res.end("Created");
+  } catch (error) {
+    console.log("Dirictory error:", error);
+  }
+  // res.end("File controller");
 };
