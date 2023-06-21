3ebb58de7c651fee5cc3db7f03ad76034a9e2696
shohan-ch <shohan889@yahoo.com>
2023-06-21 23:39:01 +0600

    Multiple listeners added for single event

--- controllers/EventController.js
+++ controllers/EventController.js
@@ -3,6 +3,12 @@ const EventEmitter = require("events");
 exports.nameEvent = (res) => {
   let eventEmitter = new EventEmitter();
 
+  eventEmitter.on("writeName", () => {
+    console.log("My name is shohan");
+  });
+  eventEmitter.on("writeName", () => {
+    console.log("123 shohan");
+  });
   eventEmitter.on("writeName", (name) => {
     res.end(`My name is ${name}`);
   });
